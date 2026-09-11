ed96c3f61e42b25e8de7f265fbd0c948e2c5860a
andrewmanzano <andoromanzano@gmail.com>
2026-09-11 19:05:57 +0800

    Question 1

---
+++ Controllers/QuestionController.cs
@@ -0,0 +1,26 @@
+﻿using Manzano_Exam.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Manzano_Exam.Controllers
+{
+    public class QuestionController : Controller
+    {
+        public ActionResult Index()
+        {
+            var questions = new List<Question>
+            {
+                new Question
+                {
+                    Number = 1,
+                    Text = "What is the main problem solved by using a database instead of an in-memory collection?",
+                    ChoiceA = "It makes C# code shorter",
+                    ChoiceB = "It prevents the application from restarting",
+                    ChoiceC = "It allows data to persist after the application stops",
+                    ChoiceD = "It removes the need for MVC",
+                    Answer = "C"
+                }
+            };
+            return View(questions);
+        }
+    }
+}
