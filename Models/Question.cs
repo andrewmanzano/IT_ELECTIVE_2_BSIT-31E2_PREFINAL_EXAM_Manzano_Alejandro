766e2625d7479695232b1506b7c2bdae908b5044
andrewmanzano <andoromanzano@gmail.com>
2026-09-11 18:56:34 +0800

    Question

---
+++ Models/Question.cs
@@ -0,0 +1,19 @@
+﻿namespace Manzano_Exam.Models
+{
+    public class Question
+    {
+        public int Number { get; set; }
+
+        public string Text { get; set; }
+
+        public string ChoiceA { get; set; }
+
+        public string ChoiceB { get; set; }
+
+        public string ChoiceC { get; set; }
+
+        public string ChoiceD { get; set; }
+
+        public string Answer { get; set; }
+    }
+}
