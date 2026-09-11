c7da760fb2da2e77d819eaed9199a1b8618a32dc
andrewmanzano <andoromanzano@gmail.com>
2026-09-11 19:20:59 +0800

    Question 3

--- Controllers/QuestionController.cs
+++ Controllers/QuestionController.cs
@@ -18,7 +18,29 @@ namespace Manzano_Exam.Controllers
                     ChoiceC = "It allows data to persist after the application stops",
                     ChoiceD = "It removes the need for MVC",
                     Answer = "C"
-                }
+                },
+
+                new Question
+                {
+                    Number = 2,
+                    Text = "Which approach is being used when an existing database is used to generate EF Core entity classes?",
+                    ChoiceA = "Code-First",
+                    ChoiceB = "Database-First",
+                    ChoiceC = "Model-First",
+                    ChoiceD = "Controller-First",
+                    Answer = "B"
+                },
+
+                new Question
+                {
+                    Number = 3,
+                    Text = "What is the primary purpose of Entity Framework Core?",
+                    ChoiceA = "To create HTML pages automatically",
+                    ChoiceB = "To replace the MVC Controller",
+                    ChoiceC = "To map objects in code to relational database data",
+                    ChoiceD = "To replace the C# compiler",
+                    Answer = "C"
+                },
             };
             return View(questions);
         }
